d0fd77ea1646aef37264b7d37430de7fba271a42
Jesús R <jde.ram9@gmail.com>
2021-12-05 19:53:49 -0600

    Reading JSON files

--- app.mjs
+++ app.mjs
@@ -1,10 +1,25 @@
 import express from 'express';
+import { readFile } from 'fs/promises';
 
 const app = express();
 
-app.use('/', (req, res) => {
-    res.send('<h1>Hola mundo</h1>');
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world</h1>');
 });
+
+app.get('/person/:user', (req, res) => {
+    res.send('Greetings ' + req.params.user);
+});
+
+app.post('/json/:message', async (req, res) => {
+    let message = req.params.message;
+    console.log(req.body);
+    let jsonfilename = 'storage/database2.json';
+    let jsoncontent = JSON.parse(await readFile(jsonfilename, 'utf8'));
+    res.json(jsoncontent[message]);
+})
 
 app.listen(3000, () => {
     console.log('Aplicación en el puerto 3000');
